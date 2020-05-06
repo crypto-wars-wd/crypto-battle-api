2bfb76294fd5c84aefc4f0b8d2e4a380eabdb94b
Borys Andrzhiievskyi <borysan@wizardsdev.com>
2020-05-06 17:13:37 +0300

    start form app

--- app.js
+++ app.js
@@ -1,41 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const bodyParser = require('body-parser');
+const { routes } = require('routes');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({ exposedHeaders: [ 'access-token', 'expires-in', 'waivio-auth', 'um_session', 'api-key', 'nightmode' ] }));
+app.use(morgan('dev'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use('/', routes);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  res.status(res.result.status || 200).json(res.result.json);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({ message: err.message });
 });
-
 module.exports = app;
