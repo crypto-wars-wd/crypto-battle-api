019f9c0a991dce6dad3aecbb7f0276fdca118022
Borys Andrzhiievskyi <borysan@wizardsdev.com>
2020-05-07 16:45:26 +0300

    render.js fix

--- concerns/render.js
+++ concerns/render.js
@@ -1,12 +1,12 @@
-const custom = (res, status, data) => res.status(status).json(data);
+const custom = (res, status, data) => res.status(status).send(data);
 
 const error = function (res, error) {
-  return res.status(422).json({ success: false, error });
+  return res.status(422).send({ success: false, error });
 };
 
-const success = (res, data) => res.status(200).json(data);
+const success = (res, data) => res.status(200).send(data);
 
-const notFound = (res, data) => res.status(404).json(data);
+const notFound = (res, data) => res.status(404).send(data);
 
 const unauthorized = (res, data) => res.status(401).send({
   success: false,
