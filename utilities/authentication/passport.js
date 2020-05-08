43064d740ba26172aef84cf12216ff2c19cbebb4
Boris Andrzhievskyi <borees2@gmail.com>
2020-05-08 19:37:29 +0300

    hide

--- utilities/authentication/passport.js
+++ utilities/authentication/passport.js
@@ -2,8 +2,8 @@ const FacebookStrategy = require('passport-facebook-token');
 const GoogleStrategy = require('passport-google-token').Strategy;
 
 module.exports = async (passport) => {
-  const facebookCredentials = { clientID: '754038848413420', clientSecret: '6d0255aa0abd3f68bce0ba4314e16dbd' };
-  const googleCredentials = { clientID: '675980225683-slfajh8g6vo2vkjd05128jvgupueqaeq.apps.googleusercontent.com', clientSecret: '0BrDlGV1qAkLPBccaE6Q5C_g' };
+  const facebookCredentials = { clientID: '', clientSecret: '' };
+  const googleCredentials = { clientID: '', clientSecret: '' };
 
   passport.use('facebook', new FacebookStrategy(facebookCredentials, getSocialFields));
   passport.use('google', new GoogleStrategy(googleCredentials, getSocialFields));
