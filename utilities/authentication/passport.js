afe9e870218158ead2bca67fc70808097ac2edac
Boris Andrzhievskyi <borees2@gmail.com>
2020-05-12 13:24:09 +0300

    add ecosystem config to gitignore

--- utilities/authentication/passport.js
+++ utilities/authentication/passport.js
@@ -3,7 +3,7 @@ const GoogleStrategy = require('passport-google-token').Strategy;
 
 module.exports = async (passport) => {
   const facebookCredentials = { clientID: process.env.FACEBOOK_CLIENT_ID || '', clientSecret: process.env.FACEBOOK_CLIENT_SECRET || '' };
-  const googleCredentials = { clientID: process.env.GOOGLE_CLIENT_ID || '', clientSecret: process.env.GOOGLE_CLIENT_SECRET };
+  const googleCredentials = { clientID: process.env.GOOGLE_CLIENT_ID || '', clientSecret: process.env.GOOGLE_CLIENT_SECRET || '' };
 
   passport.use('facebook', new FacebookStrategy(facebookCredentials, getSocialFields));
   passport.use('google', new GoogleStrategy(googleCredentials, getSocialFields));
