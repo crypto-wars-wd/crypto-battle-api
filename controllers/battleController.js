d834321d0abb4d36b32b5fb2d0db7819a0800fb0
Boris Andrzhievskyi <borees2@gmail.com>
2020-05-14 14:02:57 +0300

    add validation to create battle

--- controllers/battleController.js
+++ controllers/battleController.js
@@ -3,10 +3,10 @@ const render = require('concerns/render');
 const validators = require('./validators');
 
 const createBattle = async (req, res) => {
-  // const { params, validationError } = validators.validate(req.query, validators.authentication.hasSocialShcema);
+  const { params, validationError } = validators.validate(req.body, validators.battle.createBattleShcema);
 
-  // if (validationError) return render.error(res, validationError);
-  const result = await battleModel.createNewBattle(req.body);
+  if (validationError) return render.error(res, validationError);
+  const result = await battleModel.createNewBattle(params);
 
   return render.success(res, result);
 };
--- controllers/validators/index.js
+++ controllers/validators/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   authentication: require('./authenticationValidator'),
+  battle: require('./battleValidator'),
   validate: (data, schema) => {
     const result = schema.validate(data, { abortEarly: false });
 
