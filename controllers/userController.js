7fbcde9aa2f6d9919053217ea0059af1380dad70
Boris Andrzhievskyi <borees2@gmail.com>
2020-05-18 12:19:50 +0300

    add documentation update-user-info and add example of responses to all routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const render = require('concerns/render');
 const validators = require('./validators');
 
 const updateUserInfo = async (req, res) => {
-  const { params, validationError } = validators.validate(req.body, validators.authentication.socialAuthShcema);
+  const { params, validationError } = validators.validate(req.body, validators.user.updateInfoSchema);
   if (validationError) return render.error(res, validationError);
   const result = await userModel.updateUserInfo(params);
   return render.success(res, result);
