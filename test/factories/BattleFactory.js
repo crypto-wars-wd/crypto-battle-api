6da2c805e340528e0f65c1aa91d0da4a2b3e965d
Boris Andrzhievskyi <borees2@gmail.com>
2020-05-18 10:36:19 +0300

    add only data in factory

--- test/factories/BattleFactory.js
+++ test/factories/BattleFactory.js
@@ -1,14 +1,14 @@
 const { faker, Battle } = require('test/testHelper');
 
 const createNewBattle = async ({
-  cryptoName, playerID, healthPoints,
+  cryptoName, playerID, healthPoints, onlyData,
 } = {}) => {
   const battleData = {
     'playersInfo.firstPlayer.cryptoName': cryptoName || `${faker.name.firstName()}`,
     'playersInfo.firstPlayer.playerID': playerID || `${faker.name.firstName()}${faker.random.number()}`,
     'playersInfo.healthPoints': healthPoints || `${faker.random.number()}`,
   };
-
+  if (onlyData) return battleData;
   const battle = new Battle(battleData);
 
   await battle.save();
--- test/models/battleModel.test.js
+++ test/models/battleModel.test.js
@@ -9,11 +9,11 @@ describe('battleModel', async () => {
 
     beforeEach(async () => {
       await dropDatabase();
-      battleData = await BattleFactory.createNewBattle({ cryptoName: 'BTC' });
+      battleData = await BattleFactory.createNewBattle({ cryptoName: 'BTC', onlyData: true });
       mockData = {
-        cryptoName: battleData.battle.playersInfo.firstPlayer.cryptoName,
-        playerID: battleData.battle.playersInfo.firstPlayer.playerID,
-        healthPoints: battleData.battle.playersInfo.healthPoints,
+        cryptoName: battleData['playersInfo.firstPlayer.cryptoName'],
+        playerID: battleData['playersInfo.firstPlayer.playerID'],
+        healthPoints: battleData['playersInfo.healthPoints'],
       };
     });
     it('should save new battle to db', async () => {
@@ -22,7 +22,7 @@ describe('battleModel', async () => {
     });
     it('should find battle', async () => {
       await battleModel.createNewBattle(mockData);
-      const battle = await models.Battle.findOne({ 'playersInfo.firstPlayer.cryptoName': 'BTC' });
+      const battle = await models.Battle.findOne({ 'playersInfo.firstPlayer.playerID': mockData.playerID });
       expect(battle.playersInfo.firstPlayer.cryptoName).to.be.eq('BTC');
     });
     it('should be error message', async () => {
