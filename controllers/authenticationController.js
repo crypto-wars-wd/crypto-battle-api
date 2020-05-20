b735a98415ee921034494eb9f99e15b111296f20
Boris Andrzhievskyi <borees2@gmail.com>
2020-05-20 23:06:43 +0300

    small refactor auth controller

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -4,7 +4,6 @@ const {
 const { userModel } = require('models');
 const render = require('concerns/render');
 const { strategies } = require('utilities/operations');
-const { setAuthHeaders } = require('utilities/authentication/sessions');
 const { sessions } = require('utilities/authentication');
 const validators = require('./validators');
 
@@ -18,17 +17,18 @@ const socialSignIn = async (req, res, next) => {
 
   if (message) return render.unauthorized(res, message);
 
-  setAuthHeaders(res, user, session);
+  sessions.setAuthHeaders(res, user, session);
   return render.success(res, signInView({ user }));
 };
 
 const validateAuthToken = async (req, res) => {
-  setAuthHeaders(res, req.auth.user, req.auth.session);
+  sessions.setAuthHeaders(res, req.auth.user, req.auth.session);
   return render.success(res, validateAuthTokenView({ user: req.auth.user }));
 };
 
 const hasSocialAccount = async (req, res) => {
-  const { params, validationError } = validators.validate(req.query, validators.authentication.hasSocialShcema);
+  const { params, validationError } = validators
+    .validate(req.query, validators.authentication.hasSocialShcema);
 
   if (validationError) return render.error(res, validationError);
   const result = await userModel.findUserBySocial(params);
@@ -37,18 +37,18 @@ const hasSocialAccount = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-  let user, message;
-
-  const { params, validationError } = validators.validate(req.body, validators.authentication.logoutShcema);
+  const { params, validationError } = validators
+    .validate(req.body, validators.authentication.logoutShcema);
   if (validationError) return render.error(res, validationError);
 
   const { payload, error } = await sessions.getAuthData({ req });
   if (error) return render.unauthorized(res, error);
 
-  ({ user, message } = await userModel.findUserById(params.id));
+  const { user, message } = await userModel.findUserById(params.id);
   if (message) return render.error(res, message);
 
-  const session = sessions.findSession({ sessions: user && user.auth && user.auth.sessions, sid: payload.sid });
+  const session = sessions
+    .findSession({ sessions: user && user.auth && user.auth.sessions, sid: payload.sid });
   await userModel.destroySession({ userId: user._id, session });
   return render.success(res);
 };
