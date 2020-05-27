b29d9acbd1801e2b1c1c5eecd1ebc8616aa42221
Boris Andrzhievskyi <borees2@gmail.com>
2020-05-27 19:36:35 +0300

    exposedHeaders

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ exposedHeaders: ['access-token', 'expires-in', 'api-key'] }));
+
 app.use(morgan('dev'));
 
 app.use('/', routes);
