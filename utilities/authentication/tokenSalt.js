7088db3c474836c456c34bc254f6051216f5fa88
Boris Andrzhievskyi <borees2@gmail.com>
2020-05-28 14:30:23 +0300

    test passed

--- utilities/authentication/tokenSalt.js
+++ utilities/authentication/tokenSalt.js
@@ -1,10 +1,10 @@
 const crypto = require('crypto-js');
 
-const encodeToken = ({ accessToken }) => crypto.AES.encrypt(accessToken, process.env.CRYPTO_KEY || 'asdfsdfgdfgr').toString();
+const encodeToken = ({ accessToken }) => crypto.AES.encrypt(accessToken, process.env.CRYPTO_KEY || 'db5c57b3fc1c105e772a3784df6b798c').toString();
 
 const decodeToken = async ({ accessToken }) => {
   try {
-    return crypto.AES.decrypt(accessToken, process.env.CRYPTO_KEY || 'asdfsdfgdfgr').toString(crypto.enc.Utf8);
+    return crypto.AES.decrypt(accessToken, process.env.CRYPTO_KEY || 'db5c57b3fc1c105e772a3784df6b798c').toString(crypto.enc.Utf8);
   } catch (error) {
     return null;
   }
